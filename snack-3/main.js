ae2897d3d950ef30ee1aa9bfc0bd08eea2b89aee
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-05 14:41:36 +0200

    added array + style of divs

---
+++ snack-3/main.js
@@ -0,0 +1,15 @@
+// Partendo da un array di numeri X
+// Crea due tag div con due id diversi:
+// un div avrà il testo colorato di rosso mentre l’altro di verde.
+// stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+// Creo l'array di numeri
+const array = [10 , 20, 33, 2 , 3, 5, 7, 34, 56, 74, 73, 21]
+
+// Creo i due tag div in html e gli do stile selezionandoli tramite il loro id
+const redContainer = document.getElementById("red");
+redContainer.style.color = "red"
+const greenContainer = document.getElementById("green");
+greenContainer.style.color = "green"
+
+ 
