69d0edb91ffb3b0c8a96b9ba50cd9be51e956c02
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-05 14:52:25 +0200

    added logic, color bugged

--- snack-3/main.js
+++ snack-3/main.js
@@ -3,19 +3,21 @@
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
-// Creo l'array di numeri
-const array = [10 , 20, 33, 2 , 3, 5, 7, 34, 56, 74, 73, 21]
-
 // Creo i due tag div in html e gli do stile selezionandoli tramite il loro id
-const redContainer = document.getElementById("red");
+const redContainer = document.getElementById("red")
 redContainer.style.color = "red"
-const greenContainer = document.getElementById("green");
+const greenContainer = document.getElementById("green")
 greenContainer.style.color = "green"
 
+// Creo l'array di numeri
+const array = [10 , 20, 33, 2 , 3, 5, 7, 34, 56, 74, 73, 21]
 // scorro ogni dato dell'array con .lenght
-for (let i = 0; i < array.length){
+for (let i = 0; i < array.length; i++){
     // SE % 2 === 0 stampo i numeri nell'innerHTML del container verde
+    if(array[i] % 2 === 0){
+        greenContainer.innerHTML += array[i]
     // ALTRIMENTI in quello rosso
-
-    } 
+    } else{
+        redContainer.innerHTML += array[i]
+    }
 }
