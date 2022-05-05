83dd5e5992bb5aa706abed3a496377f74e5bccd6
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-05 14:31:00 +0200

    added random number logic

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,10 +4,17 @@
 
 // chiedo all'utente di inserire un numero
 const selectedNumber = Number(prompt("inserisci un numero"))
-
+// creo costante array vuota
 const array = []
+// creo 10 volte un array vuoto => la variabile i indica il numero di volte che viene ripetuto l'array
 for (let i = 0; i < selectedNumber; i++) {
     array[i] = [];
+// popolo l'array con 10 numeri random => la variabile x indica il numero di volte che viene ripetuto il numero casuale
+    for (let x = 0; x < 10; x++) {
+        array[i][x] = Math.floor(Math.random() * 100) + 1;
+      }
+    
 }
 
-console.log(array)
+// stampo gli array
+console.log(array);
